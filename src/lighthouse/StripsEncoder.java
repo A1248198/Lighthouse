b069ad90f54d38134d0ee2fc0ac97af11ad5e8ec
A1248198 <ageev.1248198@studenti.uniroma1.it>
2017-02-07 20:19:30 +0100

    added exist. in goal

--- src/lighthouse/StripsEncoder.java
+++ src/lighthouse/StripsEncoder.java
@@ -23,6 +23,7 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.StringWriter;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
 
@@ -32,128 +33,228 @@ import java.util.TreeSet;
  */
 public class StripsEncoder
 {
-    public static  String generateStrips(Instance i,OutputStream domain,OutputStream problem) throws IOException{
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        __generateStrips(i,baos,problem);
-        String res = baos.toString();
-        res= indent(res);
-        domain.write(res.getBytes());
-        return res;
-    }
-    private static  String __generateStrips(Instance i,OutputStream domain,OutputStream problem){
-        PrintStream ps = new PrintStream(domain);
-        ps.println("(define (domain "+i.name+")");
-        Collection<Predicate> pred = i.getPredicates();
-        ps.print("(:predicates ");
-        for (Predicate p : pred){
-            int arity = p.getArity();
-            ps.print("("+p.getName()+" ");
-            for (int j = 0; j<arity ; j+=1){
-                ps.print("?"+getLetter(j)+" ");
-            }
-            ps.print(" )");
-        }
-        ps.println(")");
+    Instance instance;
+    OutputStream domain;
+    OutputStream problem;
+    
 
-        for(Action a : i.getActions()){
-            printAction(a,ps,i);
-        }
-        ps.print(")");
-        ps.close();
-        ps = new PrintStream(problem);
-        ps.println("(define (problem "+i.name+")");
-        ps.println("(:domain "+i.name+")");
-        ps.println( "(:objects ");
-        for (String c  : i.getConstants()){
-            ps.print(c+" ");
-        }
-        ps.println(")");
-        ps.println( "(:init ");
-        for(PredicateInst p:i.getInitialState()){
-            ps.print("("+p.getName()+" ");
-            for (String param : p.getParams()){
-                
-                ps.print(param+" " );
-            }
-            ps.println(")");
-        }
-        ps.println(")");
-        ps.println( "(:goal (and");
-        for(PredicateInst p:i.getFinalState()){
-            if(p.negated){
-                ps.print("( not ");
-            }
-            ps.print("("+p.getName()+" ");
-            for (String param : p.getParams()){
-                ps.print(param+" " );
-            }
-            ps.println(")");
-            if(p.negated){
-                ps.println(")");
-            }
-        }
-        ps.println(")))");
-
-        return "";
-    }
-    private static void printParam(PrintStream ps,String param,Instance i){
-        ps.print(" ");
-        if (i.isVariable(param)){
-            ps.print("?");
-        }
-        ps.print(param);
-        ps.print(" ");
-    }
-    private static void printAction(Action a, PrintStream ps,Instance i){
-        ps.println("\n(:action "+a.name+"\n");
-        ps.print("   :parameters\n    (");
-        for(String v: a.getParameters()){
-            ps.print("?"+v+"\n    ");
-        }
-        ps.println(")\n");
-        ps.print("   :precondition\n    ( and\n    ");
-        for(PredicateInst p:a.getPre()){
-            if(p.negated){
-                ps.print("    ( not "+ "(" + p.name);
-                for (String param:p.getParams()){
-                    printParam(ps,param,i);
-                }
-                ps.print("))\n    ");
-            }
-            else{
-                ps.print("    ("+p.name);
-                for (String param:p.getParams()){
-                    printParam(ps,param,i);
-                }
-                ps.print(")\n    ");
-            }
-        }
-        ps.println(")"); //close and and preconditions
-        ps.print("   :effect\n   (and\n    ");
-        for(PredicateInst p:a.getPostNeg()){
-            
-                ps.print("    ( not "+ "(" + p.name);
-                for (String param:p.getParams()){
-                    printParam(ps,param,i);
-                }
-                ps.print("))\n    ");
-        }
-        for(PredicateInst p:a.getPostPos()){
-                ps.print("    ("+p.name);
-                for (String param:p.getParams()){
-                    printParam(ps,param,i);
-                }
-                ps.print(")\n    ");
-        }
-        ps.print(")\n)"); //close  preconditions
+    public StripsEncoder(Instance instance, OutputStream domain, OutputStream problem)
+    {
+        this.instance = instance;
+        this.domain = domain;
+        this.problem = problem;
         
     }
-    public static String getLetter(int i){
+
+    String buf ="";
+    StringBuffer res = new StringBuffer();
+    private int psPrint(String s){
+        return psPrint(s,-1);
+    } 
+    private int psPrint(String s, int indentLevel){
+        if(indentLevel<0){
+            buf+=s;
+        }
+        else{
+            String spacer = mkSpacer(indentLevel-buf.length());
+            buf+=spacer+s;
+        }
+        return buf.length();
+    }
+    private int psPrintln(String s){
+        return psPrintln(s,-1);
+        
+    } 
+    private int psPrintln(String s, int indentLevel){
+        int caret = psPrint(s,indentLevel);
+        lineBreak();
+        return caret;
+    }
+    private void lineBreak(){
+        res.append(buf).append("\n");
+        buf ="";
+    }
+        
+    PrintStream ps ;
+
+    private String postprocess(){
+        if(!buf.equals("")){
+            lineBreak();
+        }
+        return res.toString().replaceAll("\\s+\\)", ")");
+    }
+    public String generateStrips(){
+        ps = new PrintStream(domain);
+        psPrintln("(define (domain "+instance.name+")");
+        
+        mkPredicateDeclarations();
+        for(Action a : instance.getActions()){
+            printAction(a,ps,instance);
+        }
+        psPrint(")");
+        ps.print(postprocess());
+        res = new StringBuffer();
+        ps.close();
+        ps = new PrintStream(problem);
+        psPrintln("(define (problem "+instance.name+")");
+        psPrintln("(:domain "+instance.name+")");
+        mkObjects();
+        mkInitial();
+        mkFinal();
+        psPrintln(")");
+        ps.print(postprocess());
+        ps.close();
+        return "";
+    }
+    private void mkFinal(){
+        int goallvl = psPrintln( "(:goal (and");
+        for(PredicateInst p:instance.getGoalsWithoutVar()){
+            if(p.negated){
+                psPrint("( not ",goallvl);
+            }
+            else{
+                psPrint("",goallvl);
+            }
+            psPrint("("+p.getName()+" ");
+            for (String param : p.getParams()){
+                psPrint(param+" " );
+            }
+            if(p.negated){
+                psPrintln("))");
+            }
+            else{
+                psPrintln(")");
+            }
+        }
+        if(!instance.getVariablesInFinalState().isEmpty()){
+            mkExistentialFinal(goallvl);
+        }
+        psPrintln("))");
+    }
+    private void mkExistentialFinal(int startlvl){
+        int existlvl = psPrintln( "(exists (",startlvl);
+        for(String var : instance.getVariablesInFinalState()){
+            psPrint("?"+var+" ");
+        }
+        psPrintln(") (and");
+        printPreds(instance.getGoalsWithVar(),existlvl);
+        
+        psPrintln("))");
+    }
+    private void printPreds(List<PredicateInst> what, int lvl){
+        for(PredicateInst p:what){
+            if(p.negated){
+                psPrint("( not ",lvl);
+            }
+            else{
+                psPrint("",lvl);
+            }
+            psPrint("("+p.getName()+" ");
+            for (String param : p.getParams()){
+                if(instance.isConstant(param))
+                    psPrint(param+" " );
+                else
+                    psPrint("?"+param+" ");
+                
+            }
+            if(p.negated){
+                psPrintln("))");
+            }
+            else{
+                psPrintln(")");
+            }
+        }
+    }
+    private void mkInitial(){
+        int ilvl = psPrintln( "(:init ");
+        for(PredicateInst p:instance.getInitialState()){
+            psPrint("("+p.getName()+" ",ilvl);
+            for (String param : p.getParams()){
+                
+                psPrint(param+" " );
+            }
+            psPrintln(")");
+        }
+        psPrintln(")",ilvl);
+    }
+    private  void printParam(String param){
+        psPrint(" ");
+        if (instance.isVariable(param)){
+            psPrint("?");
+        }
+        psPrint(param);
+        psPrint(" ");
+    }
+    private void mkObjects(){
+        int olvl = psPrintln( "(:objects ");
+        for (String c  : instance.getConstants()){
+            psPrintln(c,olvl);
+        }
+        psPrintln(")",olvl);
+    }
+    private void mkPredicateDeclarations(){
+        Collection<Predicate> pred = instance.getPredicates();
+        int lvl = psPrint("(:predicates ");
+        for (Predicate p : pred){
+            int arity = p.getArity();
+            psPrint("("+p.getName()+" ",lvl);
+            for (int j = 0; j<arity ; j+=1){
+                psPrint("?"+getLetter(j)+" ");
+            }
+            psPrintln(")");
+        }
+        psPrintln(")",lvl);
+    }
+    private  void printAction(Action a, PrintStream ps,Instance i){
+        int actlvl = psPrintln("(:action "+a.name+"");
+        int parlvl = psPrint(":parameters (",actlvl);
+        for(String v: a.getParameters()){
+            psPrintln("?"+v+"",parlvl);
+        }
+        psPrintln(")",parlvl);
+        int prelvl = psPrint(":precondition ( and ",actlvl);
+        for(PredicateInst p:a.getPre()){
+            if(p.negated){
+                psPrint("( not "+ "(" + p.name,prelvl);
+                for (String param:p.getParams()){
+                    printParam(param);
+                }
+                psPrintln("))");
+            }
+            else{
+                psPrint("("+p.name,prelvl);
+                for (String param:p.getParams()){
+                    printParam(param);
+                }
+                psPrintln(")");
+            }
+        }
+        psPrintln(")",prelvl); //close and and preconditions
+        int postlvl = psPrint(":effect ( and ",actlvl);
+        for(PredicateInst p:a.getPostNeg()){
+            
+                psPrint("( not "+ "(" + p.name,postlvl);
+                for (String param:p.getParams()){
+                    printParam(param);
+                }
+                psPrintln("))");
+        }
+        for(PredicateInst p:a.getPostPos()){
+                psPrint("("+p.name,postlvl);
+                for (String param:p.getParams()){
+                    printParam(param);
+                }
+                psPrintln(")");
+        }
+        psPrintln("))",actlvl); //close postconditions
+        
+    }
+    public  String getLetter(int i){
         char c = Character.forDigit(i+10, 36);
         return ""+c;
     }
 
-    private static String indent(String res)
+    private  String indent(String res)
     {
         
         return res;
@@ -162,7 +263,7 @@ public class StripsEncoder
     private String mkSpacer(int level){
         String res = "";
         for(int i = 0;i<level;i+=1){
-            res += "    ";
+            res += " ";
         }
         return res;
     }
