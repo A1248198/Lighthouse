8c08982dd5678c27fa8b4251d1fb024aa8545f96
A1248198 <ageev.1248198@studenti.uniroma1.it>
2017-03-27 00:51:58 +0200

    minor fix

--- Scripts/integers.lua
+++ Scripts/integers.lua
@@ -3,18 +3,24 @@
 addPredicate('integer',1)
 addPredicate('zero',1)
 addPredicate('succ',2)
+
+
 function mkInteger(prefix, upto)
+    if(upto < 0 ) then
+        error("upto should not be negative")
+    end
     for i=0, upto do
         name =  prefix .. i
         addConstant(name)
         setInitial('integer',{name})
-        if(i == 0) then
-            setInitial('zero',{name})
-        end
     end
+    setInitial('zero', {prefix .. 0})
 end
 
 function lessUpTo(prefix, upto)
+    if(upto < 0 ) then
+        error("upto should not be negative")
+    end
     addPredicate('less',2)
     for i=0, upto  do
         for j=0, i-1 do
@@ -24,6 +30,9 @@ function lessUpTo(prefix, upto)
 end
 
 function mkSucc(prefix,upto)
+    if(upto < 0 ) then
+        error("upto should not be negative")
+    end
     addPredicate('succ',2)
     for i = 0, upto -1 do
         setInitial('succ',{prefix..i,prefix..(i+1)})
